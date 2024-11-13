cba987c7e595d15622488f5c591d74a6b6f291b6
Junming Huang <junming.huang@d1m.cn>
2024-11-13 13:42:31 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      data-lt-installed="true"  // to prevent Hydration errors caused by LanguageTool extension: https://github.com/facebook/react/issues/24430
+    >
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
